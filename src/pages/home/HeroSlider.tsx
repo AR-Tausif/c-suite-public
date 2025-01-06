23af3e27a950bfa3a42cf1f7c4d27f3b6f97423f
tausif.ritu1 <tausif.ritu1@gmail.com>
2025-01-06 08:59:30 +0000

    updated slider to responsive slider in home page

--- src/pages/home/HeroSlider.tsx
+++ src/pages/home/HeroSlider.tsx
@@ -1,44 +1,30 @@
-import * as React from "react";
-import Autoplay from "embla-carousel-autoplay";
 
-import { Card, CardContent } from "@/components/ui/card";
 import {
   Carousel,
   CarouselContent,
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel";
+} from "@/components/ui/carousel"
 
-export const HeroSlider = () => {
-  const plugin = React.useRef(
-    Autoplay({ delay: 2000, stopOnInteraction: true })
-  );
-
+export function HeroSlider() {
   return (
-    <div className="flex relative justify-center max-w-[87rem] overflow-hiddenk bg-black h-screen">
-        <Carousel
-      plugins={[plugin.current]}
-      className="w-full max-w-sm"
-      onMouseEnter={plugin.current.stop}
-      onMouseLeave={plugin.current.reset}
-    >
+    <Carousel className="w-screen max-h-screen overflow-hidden">
       <CarouselContent>
         {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
-                </CardContent>
-              </Card>
+          <CarouselItem key={index} className="w-full h-full">
+            {/* <div className="flex flex-col items-center justify-center w-full h-full bg-gradient-to-r from-purple-500 to-pink-500">
+              
+            </div> */}
+            <div className="relative w-full h-full">
+              <img src="https://t4.ftcdn.net/jpg/05/37/32/57/360_F_537325726_GtgjRiyc37BLPn9OmisBVVZec9frLaL0.jpg" width={"100%"} height="80vh" alt="" />
             </div>
           </CarouselItem>
         ))}
       </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
+      <CarouselPrevious className="left-4" />
+      <CarouselNext className="right-4" />
     </Carousel>
-    </div>
-  );
-};
+  )
+}
+
