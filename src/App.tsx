eefa18bc69de9d2eb76027b56bcc59bc67367eec
tausif.ritu1 <tausif.ritu1@gmail.com>
2025-01-16 20:45:13 +0000

    about c suite section fixed with given red line border

--- src/App.tsx
+++ src/App.tsx
@@ -16,9 +16,20 @@ const App = () => {
     <>
       <ResponsiveNavbar />
       <HeaderHero />
+      
       <Container>
         <div className="flex justify-between items-center py-10 border-b">
-          <h2 className="text-4xl font-bold">WHAT IS C- SUITE?</h2>
+        <div className="relative inline-block text-center">
+        {/* <!-- Red borders --> */}
+        <div className="absolute top-0 left-0 h-2 w-28 bg-red-600"></div>
+        <div className="absolute top-0 left-0 h-16 w-2 bg-red-600"></div>
+        
+        <div className="absolute bottom-0 right-0 h-2 w-28 bg-red-600"></div>
+        <div className="absolute bottom-0 right-0 h-16 w-2 bg-red-600"></div>
+        
+        {/* <!-- Text --> */}
+          <h2 className="text-4xl font-bold p-5 px-8">WHAT IS C- SUITE?</h2>
+    </div>
           <p className="w-2/4 text-gray-600">
             A dynamic partner of engagement, placement, assessment, recruitment
             and development solutions, delivering unparalleled C-Level
