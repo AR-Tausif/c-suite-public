1389d8af0a6a7b2a4ca276c50cdb153cba826eb7
tausif.ritu1 <tausif.ritu1@gmail.com>
2025-01-18 03:42:06 +0000

    deleted unnecessary files

--- src/pages/home/SectionHeading.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import { HeadingProps } from "./Types";
-
-export const SectionHeading: React.FC<HeadingProps> = ({ primary, secondary }) => (
-  <div className="flex gap-3 text-4xl font-extrabold whitespace-nowrap">
-    <div className="text-neutral-900">{primary}</div>
-    <p className="basis-auto text-4xl font-extrabold bg-gradient-to-tl from-purple-600 to-red-500 bg-clip-text text-transparent">{secondary}</p>
-  </div>
-);
